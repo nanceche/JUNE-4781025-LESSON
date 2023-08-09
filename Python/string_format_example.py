dd69605a283be6c60ea4f19ba4434666cf55371a
Leon <leon.robinson@qa.com>
2023-08-09 11:52:07 +0100

    added str example

--- Python/string_format_example.py
+++ Python/string_format_example.py
@@ -1,4 +1,4 @@
-#import string
+import string
 
 var1 = "Leon"
 var2 = True
@@ -6,7 +6,10 @@ var3 = 156
 
 # print(var1 + var2 + var3)  # his wont work
 
+# Set up an output template
 outputstr  = "This var1: {}, This is var2: {}, this is var3: {} "
-
+# substitute the variables in the template
 print(outputstr.format(var1,var2,var3))
 
+
+print(str.format(outputstr,var1,var2,var3))
